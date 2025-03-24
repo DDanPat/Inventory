aeb173c7f1418013b84eede8b8ef5dd1c3d6debe
DDanPat <hyoung6855@gmail.com>
2025-03-24 20:51:45 +0900

    [chore] 인벤토리 슬롯 구성

--- Assets/Scripts/UI/UIInventory.cs
+++ Assets/Scripts/UI/UIInventory.cs
@@ -2,21 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class UIInventory : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI inventorySlotNum;
+    private int itemNum = 0;
+    [SerializeField] private UISlot slotPrefab;
+    [SerializeField] private Transform slotParent;
+    [SerializeField] private List<UISlot> slots = new List<UISlot>();
+    [SerializeField] private int slotCount = 21;
+
     [SerializeField] private Button inventoryButton;
     private void Start()
     {
         gameObject.SetActive(false);
-
         inventoryButton.onClick.AddListener(onClickInventoryButton);
+        InitializeSlots();
+    }
 
+    private void InitializeSlots()
+    {
+        inventorySlotNum.text = $"Inventory\n[ {itemNum} / {slotCount} ]";
+        for (int i = 0; i < slotCount; i++)
+        {
+            UISlot slot = Instantiate(slotPrefab, slotParent);
+            slots.Add(slot);
+        }
     }
 
     private void onClickInventoryButton()
     {
         UIManager.Instance.uiMainMenu.OpenInventory();
     }
-
 }
---
+++ Assets/Scripts/UI/UISlot.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UISlot : MonoBehaviour
+{
+    public Button uiSlotButton;
+    public TextMeshProUGUI equipText;
+    public Image icon;
+    
+}
